815cb4da926d51ffd7530cd473c8956f689f5204
mrpbueno <mrpbueno@gmail.com>
2019-07-03 20:05:58 -0300

    update route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Auth::routes();
 Route::middleware(['auth'])->group(function() {
     Route::get('/', 'ContactController@index')->name('contact.index');
     Route::get('/create', 'ContactController@create')->name('contact.create');
-    Route::post('/', 'ContactController@store')->name('contact.store');
+    Route::post('/store', 'ContactController@store')->name('contact.store');
     Route::get('/{contact}/edit', 'ContactController@edit')->name('contact.edit');
     Route::put('/{contact}', 'ContactController@update')->name('contact.update');
     Route::delete('/{contact}', 'ContactController@destroy')->name('contact.destroy');
